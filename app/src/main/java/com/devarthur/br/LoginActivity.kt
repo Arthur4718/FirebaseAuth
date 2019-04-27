074a5154d6f213250623924d30fecf0cef3ccc71
Arthur <arthur.gomes_4718@hotmail.com>
2019-04-26 23:28:25 -0300

    Selecting an image from external storage
    Using READ_EXTERNAL_STORAGE permision

--- app/src/main/java/com/devarthur/br/LoginActivity.kt
+++ app/src/main/java/com/devarthur/br/LoginActivity.kt
@@ -24,7 +24,7 @@ class LoginActivity : AppCompatActivity() {
 
 
         btnGobackToLogin.setOnClickListener {
-//            val intent = Intent(this, MainActivity::class.java)
+//            val intent = Intent(this, RegisterActivity::class.java)
 
 
               finish()
--- app/src/main/java/com/devarthur/br/RegisterActivity.kt
+++ app/src/main/java/com/devarthur/br/RegisterActivity.kt
@@ -1,9 +1,13 @@
 package com.devarthur.br
 
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import com.google.firebase.FirebaseApp
@@ -15,8 +19,7 @@ import kotlinx.android.synthetic.main.activity_main.* // Kotlin syntetic referen
 //https://www.youtube.com/watch?v=RYyri2W3Tho&list=PL0dzCUj1L5JE-jiBHjxlmXEkQkum_M3R-&index=2
 
 
-
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,15 +37,40 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        btnSelectButton.setOnClickListener {
+
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
+
+
+        }
+
+    }
+
+    var selectedPhotoUri: Uri? = null
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
+
+            selectedPhotoUri = data.data
+
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedPhotoUri)
+
+            val bitmapDrawable = BitmapDrawable(bitmap)
+            btnSelectButton.setBackgroundDrawable(bitmapDrawable)
+        }
     }
 
     //Creates a new user for firebase auth database. Returns error for invalid usernames or e-mails
-    private fun performRegister(){
+    private fun performRegister() {
 
         val email = edt_useremail.text.toString()
         val password = edt_userPassword.text.toString()
 
-        if(email.isEmpty() || password.isEmpty()) {
+        if (email.isEmpty() || password.isEmpty()) {
             Toast.makeText(this, "Please fill all the fields for registration", Toast.LENGTH_SHORT).show()
 
             return
@@ -52,13 +80,21 @@ class MainActivity : AppCompatActivity() {
 
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener {
-                if(!it.isSuccessful) return@addOnCompleteListener
+                if (!it.isSuccessful) return@addOnCompleteListener
 
                 //if successul
                 Log.d("Main", "User created with uid: ${it.result.user.uid}")
+
+                uploadImageToFirebase()
+
             }.addOnFailureListener {
                 Log.d("Main", "Failed to create user : ${it.message}")
             }
+
+    }
+
+    //Sends the image selected from the user to our firebase server
+    private fun uploadImageToFirebase(){
 
     }
 }
